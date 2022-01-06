fac4e6857c4f6a92c8ef4746550f7b351f2750af
Pablo Vega Uceta <pablovegau@gmail.com>
2022-01-06 22:56:31 +0100

    prepare pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,16 @@
-// // Inside vue.config.js
-// module.exports = {
-//   // ...other vue-cli plugin options...
-//   pwa: {
-//     name: 'My App',
-//     themeColor: '#4DBA87',
-//     msTileColor: '#000000',
-//     appleMobileWebAppCapable: 'yes',
-//     appleMobileWebAppStatusBarStyle: 'black',
-
-//     // configure the workbox plugin
-//     workboxPluginMode: 'InjectManifest',
-//     workboxOptions: {
-//       // swSrc is required in InjectManifest mode.
-//       swSrc: 'dev/sw.js'
-//       // ...other Workbox options...
-//     }
-//   }
-// }
+module.exports = {
+  pwa: {
+    name: 'Petinder',
+    themeColor: '#5B49FE',
+    msTileColor: '#7DE88C',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  }
+}
